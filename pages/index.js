ed94f104f668564f2a471de3ba4aa540f92499b9
Phutthichod <pin2017create@outlook.com>
2021-01-28 16:30:55 +0700

    update code

--- pages/index.js
+++ pages/index.js
@@ -2,93 +2,87 @@ import Link from 'next/link'
 import { useEffect, useState } from 'react'
 import CreateName from './createNewName'
 import fire from '../config/fire-config'
-// import liff from '@line/liff/dist/lib'
 import { useRouter } from 'next/router'
 const db = fire.firestore()
 
 export default function Home() {
-  console.log("new")
+  // console.log("new")
   const router = useRouter()
+  const [profileUser, setProfileUser] = useState({})
+  const [liff, setLiff] = useState(null)
   const [state, setState] = useState({})
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(false)
   const [isNewUser, setIsNewUser] = useState(true)
   const [name, setName] = useState("")
 
   const onChangeName = (name) => {
-    console.log("setname")
     setName(name)
   }
-  const addNewUser = () => {
-    // loadData()
-    console.log(state, name)
+  const addNewUserInFirebase = () => {
     db
       .collection('user')
-      .doc(state.userId).set({
-        ...state, name
+      .doc(profileUser.userId).set({
+        name
       }).then(() => {
         console.log("success")
-        setState({ name, ...state })
+        setProfileUser({ ...profileUser, name })
         setIsNewUser(false)
       }).catch(function (error) {
         console.error("Error writing document: ", error);
       });
-
-
   }
-
-  const loadData = async () => {
-    const data = await liff.getProfile()
-    // console.log(data)
-    setState(data)
-    var docRef = db.collection("user").doc(data.userId);
+  const addOrGetUserInFirebaseAndSetUserState = async (profile) => {
+    console.log("addOrGetUserInFirebaseAndSetUserState")
+    var docRef = db.collection("user").doc(profile.userId);
     docRef.get().then(function (doc) {
+      console.log(doc.data())
       if (doc.exists) {
-        console.log("have", doc.data())
-        // console.log("Document data:", doc.data());
-        setState({ ...data, ...doc.data() })
+        setProfileUser({ ...profile, ...doc.data() })
         setIsNewUser(false)
-        setLoading(false)
-      } else {
-        console.log("not have")
-        setIsNewUser(true)
-        setLoading(false)
-        // router.push("/createName")
       }
     }).catch(function (error) {
       console.log("Error getting document:", error);
     });
   }
-  useEffect(async () => {
-    fetch("/api/hello").then(res => res.json()).then(res => {
-      console.log(res)
-    })
-    const { default: liff } = await import("@line/liff");
+  const importLineLIFF = async (liff) => {
+    setLiff(liff)
+  }
+  const initLineLiff = async (liff, liffId) => {
     await liff.init({
-      liffId: "1655538913-PnDo5YK0" // Use own liffId
+      liffId // Use own liffId
     })
-
-    if (liff.isLoggedIn()) {
-
-      loadData()
+  }
+  const gotoLogin = (liff) => {
+    liff.login()
+  }
+  const checkIsLoginLine = async (liff) => {
+    const profile = await liff.getProfile()
+    setProfileUser({ ...profile })
+    if (!liff.isLoggedIn()) {
+      gotoLogin(liff)
     } else {
-      liff.login()
+      await addOrGetUserInFirebaseAndSetUserState(profile)
     }
+  }
 
-
-  }, [])
+  useEffect(async () => {
+    const { default: liff } = await import("@line/liff");
+    await importLineLIFF(liff)
+    await initLineLiff(liff, "1655538913-PnDo5YK0")
+    await checkIsLoginLine(liff)
+  }, [router])
   return (
     <div>
       { loading ? <h3>loading .... </h3> :
         <>
           {
-            isNewUser ? <CreateName onChangeName={onChangeName} onSubmit={addNewUser} /> :
+            isNewUser ? <CreateName onChangeName={onChangeName} onSubmit={addNewUserInFirebase} /> :
               <div>
-                {/* <Link href="/createName">go to new name</Link> */}
-                <h2>Id: {state.userId}</h2>
-                <h2>NameLine: {state.displayName}</h2>
-                <h2>NameNocnoc: {state.name}</h2>
-                <img src={state.pictureUrl} style={{ height: 50, width: 50 }} />
-                <h2>status {state.statusMessage}</h2>
+                <h2>Id: {profileUser.userId}</h2>
+                <h2>NameLine: {profileUser.displayName}</h2>
+                <h2>NameNocnoc: {profileUser.name}</h2>
+                <img src={profileUser.pictureUrl} style={{ height: 50, width: 50 }} />
+                <h2>status {profileUser.statusMessage}</h2>
                 <button onClick={() => {
                   liff.logout()
                   liff.login()
