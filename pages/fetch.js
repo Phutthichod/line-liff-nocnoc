d67a82b8028c9adbc3e9d07a4026fd90a9866d9b
Phutthichod <pin2017create@outlook.com>
2021-03-29 20:43:02 +0700

    fix bug useState

--- pages/fetch.js
+++ pages/fetch.js
@@ -2,7 +2,7 @@ import { responsiveFontSizes } from '@material-ui/core'
 import React from 'react'
 import { customAxios } from '../functions/axios'
 export default function fetch() {
-    const [order, setOrder] = useState([])
+    const [order, setOrder] = React.useState([])
     React.useEffect(() => {
         customAxios.get("https://3edec48878c4.ngrok.io/api/v1/survey/orders").then(res => {
             console.log(res)
