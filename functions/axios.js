9bc30cd04155da53d4bd12196af6a6595e206c10
Phutthichod <pin2017create@outlook.com>
2021-03-29 20:24:54 +0700

    update axios custom

--- functions/axios.js
+++ functions/axios.js
@@ -1,16 +1,24 @@
 import axios from 'axios'
 import fire from '../config/fire-config'
-axios.interceptors.request.use(function (config) {
+
+const axiosCustom = axios.create({
+    baseURL: ""
+});
+
+axiosCustom.interceptors.request.use(function (config) {
     const token = localStorage.getItem("access_token")
     config.headers.Authorization = `Bearer ${token}`;
 
     return config;
 });
-axios.interceptors.response.use(function (response) {
+axiosCustom.interceptors.response.use(function (response) {
+    console.log(response)
     return response;
 }, async function (error) {
     const status = error.response ? error.response.status : null
-    if (401 === status) {
+    const originalRequest = error.config;
+    if (401 === status && !originalRequest._retry) {
+        originalRequest._retry = true;
         try {
             const { default: liff } = await import("@line/liff");
             await liff.init({
@@ -32,8 +40,8 @@ axios.interceptors.response.use(function (response) {
                             fire.auth().currentUser.getIdToken(/* forceRefresh */ true).then(function (idToken) {
                                 console.log(idToken)
                                 localStorage.setItem("access_token", idToken)
-                                error.config.headers['Authorization'] = 'Bearer ' + idToken;
-                                return axios.request(error.config);
+                                axios.defaults.headers.common['Authorization'] = 'Bearer ' + idToken;
+                                return axios(originalRequest)
                             }).catch(function (error) {
                                 // Handle error
                                 window.location.href("/")
@@ -57,8 +65,7 @@ axios.interceptors.response.use(function (response) {
         }
 
 
-    } else {
-        return Promise.reject(error);
     }
+    return Promise.reject(error);
 });
-export const customAxios = axios
+export const customAxios = axiosCustom
