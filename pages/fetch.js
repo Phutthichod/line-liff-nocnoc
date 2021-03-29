6bba8487f6e2c48fa82b02f52f79091375e4c13d
Phutthichod <pin2017create@outlook.com>
2021-03-29 20:39:50 +0700

    show id item

--- pages/fetch.js
+++ pages/fetch.js
@@ -1,16 +1,24 @@
+import { responsiveFontSizes } from '@material-ui/core'
 import React from 'react'
 import { customAxios } from '../functions/axios'
 export default function fetch() {
+    const [order, setOrder] = useState([])
     React.useEffect(() => {
         customAxios.get("https://3edec48878c4.ngrok.io/api/v1/survey/orders").then(res => {
             console.log(res)
+            if (responsiveFontSizes.status === 200) {
+                setOrder(res.data)
+            }
+
         }).catch(err => {
             console.log(err)
         })
     })
     return (
         <div>
-
+            {order.map(item => {
+                return <p>{item.id}</p>
+            })}
         </div>
     )
 }
